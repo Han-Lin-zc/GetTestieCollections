cf58e44db3f9092e19da92e51afad5ada7cc18ba
hlin <Zxsaqw12@&$>
2020-03-29 10:57:09 -0400

    Add to ArrayList and HashSet tests

--- gettestie/src/test/java/rocks/zipcode/ArrayListTest.java
+++ gettestie/src/test/java/rocks/zipcode/ArrayListTest.java
@@ -1,4 +1,39 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class ArrayListTest {
+
+    ArrayList<Person> list;
+    Person person1 = new Person("Han", 1993);
+    Person person2 = new Person("Lin", 1999);
+    Person person3 = new Person("Sang", 2001);
+
+    @Before
+    public void setUp() {
+        list = new ArrayList<>();
+    }
+
+    @Test
+    public void addTest() {
+        list.add(person1);
+        list.add(person2);
+        list.add(person3);
+        int expectedCount = 3;
+        Assert.assertEquals(expectedCount, list.size());
+    }
+
+    @Test
+    public void removeAllTest() {
+        list.add(person1);
+        list.add(person2);
+        list.add(person3);
+        int expectedCount = 0;
+        list.clear();
+        Assert.assertEquals(expectedCount, list.size());
+    }
 }
--- gettestie/src/test/java/rocks/zipcode/HashSetTest.java
+++ gettestie/src/test/java/rocks/zipcode/HashSetTest.java
@@ -1,4 +1,54 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashSet;
+
 public class HashSetTest {
+
+    HashSet<String> set;
+
+    @Before
+    public void setUp() {
+        set = new HashSet<>();
+    }
+
+    @Test
+    public void daysOfTheWeek() {
+        set.add("Monday");
+        set.add("Tuesday");
+        set.add("Wednesday");
+        set.add("Thursday");
+        set.add("Friday");
+        set.add("Saturday");
+        set.add("Sunday");
+
+        Integer expected = 7;
+        Integer actual = set.size();
+        Assert.assertEquals(expected, actual);
+
+        set.clear();
+        Integer expectedAfterClear = 0;
+        Integer actualAfterClear = set.size();
+
+        Assert.assertEquals(expectedAfterClear, actualAfterClear);
+    }
+
+    @Test
+    public void doNotRepeat() {
+        set.add("Do");
+        set.add("Do");
+        set.add("Not");
+        set.add("Not");
+        set.add("Repeat");
+        set.add("Repeat");
+        set.add("Do");
+        set.add("Not");
+
+        Integer expected = 3;
+        Integer actual = set.size();
+        Assert.assertEquals(expected, actual);
+    }
 }
